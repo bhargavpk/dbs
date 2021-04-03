400218c55c9c36ff1e3b1d414e20e531315c0ad1
bhargavpk <bpk19900@gmail.com>
2021-04-03 18:18:46 +0530

    Modified package.json

--- src/nav.js
+++ src/nav.js
@@ -9,7 +9,7 @@ function Navig()
   return (
     
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -26,7 +26,7 @@ function Navig()
         <a class="nav-link" href="/Fac">Faculty</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="#">Inst. Login</a>
+        <a class="nav-link" href="/">Inst. Login</a>
       </li>
     </ul>
     
